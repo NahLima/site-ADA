9edbec13018b134811dde9b69f905bbd745fcfab
NahLima <naiarafx@gmail.com>
2020-11-19 14:01:06 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-document.getElementById("botaoEnviar").addEventListener("click", validarFormulario )
+document.getElementById("botaoEnviar").addEventListener("click", validarFormulario)
 
-function validarFormulario(){
-    if(document.getElementById("name").value !="" && document.getElementById("email").value !="" && document.getElementById("msg").value !=""){
-        alert(" Contato recebido,você recebera as novidades por email") 
-      }else{
+function validarFormulario() {
+    if (document.getElementById("name").value != "" &&
+        document.getElementById("email").value != "" &&
+        document.getElementById("msg").value != "") {
+        alert(" Contato recebido,você recebera as novidades por email")
+    } else {
         alert("Por favor insira os dados corretamente!")
-      }
+    }
 }
