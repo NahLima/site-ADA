06568f235b90a7b049d9014afb478f5cc78707a4
NahLima <naiarafx@gmail.com>
2020-11-19 13:45:43 -0300

    continuação

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("botaoEnviar").addEventListener("click", validarFormulario )
+
+function validarFormulario(){
+    if(document.getElementById("name").value !="" && document.getElementById("email").value !="" && document.getElementById("msg").value !=""){
+        alert(" Contato recebido,você recebera as novidades por email") 
+      }else{
+        alert("Por favor insira os dados corretamente!")
+      }
+}
